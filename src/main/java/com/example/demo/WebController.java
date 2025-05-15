e0bbe147166896077548153f7e32b923a0e46ccc
lauramelissa661 <Lauramelissa661@gmail.com>
2025-05-15 23:07:03 +0200

    initial

--- src/main/java/com/example/demo/WebController.java
+++ src/main/java/com/example/demo/WebController.java
@@ -4,19 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Welcome to our Java Web App!");
+        model.addAttribute("message", "Welcome!");
         return "index";
     }
 
     @GetMapping("/greet/{name}")
     public String greet(@PathVariable String name, Model model) {
         model.addAttribute("message", "Hello, " + name + "!");
-        return "index";
+        return "greet";
     }
 }
